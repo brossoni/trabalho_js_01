47b848c8bbb42f03b2558267ddcee9923c59e30f
alinecruz87 <59289746+alinecruz87@users.noreply.github.com>
2020-04-06 15:12:07 -0300

    Todos os arquivos
    finalizado css confirmacao

---
+++ js/confirm.js
@@ -0,0 +1,18 @@
+function lerParams() {
+    const params = new URLSearchParams(document.location.search.substring(1));
+    const nome = params.get("nome").toLocaleUpperCase(); 
+    const endereco = params.get("endereco");
+    const cidade = params.get("cidade");
+    const estado = params.get("estado");
+    const user = params.get("user");
+    const senha = params.get("senha2");
+    
+    document.getElementById("c_nome").innerHTML = nome;
+    document.getElementById("c_endereco").innerHTML = endereco;
+    document.getElementById("c_cidade").innerHTML = cidade;
+    document.getElementById("c_estado").innerHTML = estado;
+    document.getElementById("c_user").innerHTML = user;
+    document.getElementById("c_senha").innerHTML = senha;
+    
+}
+   
